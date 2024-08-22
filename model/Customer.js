ccb745700e867c122ad7fc0124edecef5d01cd37
someshwarsholkar <someshwarsholkar22@gmail.com>
2024-08-22 21:33:33 +0530

    Fix Modal bugs

--- model/Customer.js
+++ model/Customer.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, },
     mobile: { type: Number, required: true },
-    // password: { type: String, required: true },
     address: { type: String, },
     userId: { type: mongoose.Types.ObjectId, required: true }
 })
